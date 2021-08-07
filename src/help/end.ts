b02eb83febfab733e90b3df3e3450b720e140f1b
Miroslav Vršecký <office@yamiteru.xyz>
2021-08-07 15:18:46 +0200

    Shake off a few bytes

--- src/help/end.ts
+++ src/help/end.ts
@@ -1,15 +1,17 @@
 import {EndSubscribers, ErrSubscribers, PubSubscribers} from "../types/help";
 import pub from "./pub";
 
+const CLEAR = "clear";
+
 const end = <T>(
     pubSubscribers: PubSubscribers<T>,
     errSubscribers: ErrSubscribers,
     endSubscribers: EndSubscribers
 ) => () => {
     pub(endSubscribers)();
-    pubSubscribers.clear();
-    errSubscribers.clear();
-    endSubscribers.clear();
+    pubSubscribers[CLEAR]();
+    errSubscribers[CLEAR]();
+    endSubscribers[CLEAR]();
 };
 
 export default end;
--- src/help/pub.ts
+++ src/help/pub.ts
@@ -2,13 +2,11 @@ import {ErrSubscribers, Subscribers} from "../types/help";
 import {Maybe} from "../types/core";
 
 const pub = <T>(subscribers: Subscribers<any>, errSubscribers?: ErrSubscribers) => (value?: Maybe<T>) => {
-    for (const callback of subscribers) {
-        try {
-            callback(value);
-        } catch ({ message }) {
-            if(errSubscribers) pub(errSubscribers)(message);
-            else throw new Error(message);
-        }
+    for (const callback of subscribers) try {
+        callback(value);
+    } catch ({ message }) {
+        if(errSubscribers) pub(errSubscribers)(message);
+        else throw new Error(message);
     }
 };
 
