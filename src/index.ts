78d67c9e6f16d89b33f8b8cea698e53a8eea6c8d
Miroslav Vršecký <office@yamiteru.xyz>
2021-08-09 15:27:04 +0200

    Export public API

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export {NONE} from "./constants";
+export {stream} from "./primitives/stream";
+export {derivedStream} from "./primitives/derivedStream";
+export {value} from "./primitives/value";
+export {derivedValue} from "./primitives/derivedValue";
