187b9b88b26c689da51b7b7ec2a96aeea98ee0b4
Miroslav Vršecký <office@yamiteru.xyz>
2021-11-17 00:23:06 +0100

    Better types for deriveAny

--- src/help/derivedAny.ts
+++ src/help/derivedAny.ts
@@ -1,36 +1,36 @@
-import { 
-    Map, 
-    Maybe,
-    Noop,
-    Observable, 
-    ObservableOutputs, 
-    Some, 
-    Stream, 
-    Subs, 
-    Value 
+import {
+  Map,
+  Maybe,
+  Noop,
+  Observable,
+  ObservableOutputs,
+  Some,
+  Stream, StreamInstance,
+  Subs,
+  Value, ValueInstance,
 } from "../types";
 
 export default <
     O extends Some,
     A extends Observable<any, any>[],
     I extends ObservableOutputs<A>,
+    P extends Value | Stream,
 >(
-    primitive: Value | Stream,
-    initial: O | undefined,
+    primitive: P,
+    initial: P extends Value ? O : undefined,
     observables: [...A],
-    map: Map<I, O, O> | Map<I, O, undefined>,
+    map: P extends Value ? Map<I, O, O> : Map<I, O, undefined>,
     subs?: Maybe<Subs<O>>
 ) => {
     const l = observables.length;
-    const primitive$ = initial === undefined 
-        ? (primitive as Stream)<I, O>(map as any, subs)
-        : (primitive as Value)<I, O>(initial as any, map as any, subs);
+    const primitive$ = initial === undefined
+        ? (primitive as Stream)<I, O>(map as any, subs) as StreamInstance<I, O>
+        : (primitive as Value)<I, O>(initial as any, map as any, subs) as ValueInstance<I, O>;
     const { next, ...withoutNext } = primitive$;
     const values = [] as unknown as I;
     const unsubs = [] as unknown as Noop[];
 
-    let i = -1;
-    while(++i < l) {
+    for(let i = 0; i < l; ++i) {
         const o$ = observables[i];
         if(o$?.get) values[i] = o$?.get();
 
@@ -42,11 +42,12 @@ export default <
 
     return {
         ...withoutNext,
-        end: () => {            
-            let i = -1;
-            while(++i < l) unsubs[i]();
+        end: () => {
+            for(let i = 0; i < l; ++i) unsubs[i]();
 
             primitive$.end();
         }
-    };
+    } as P extends Value
+      ? ValueInstance<I, O>
+      : StreamInstance<I, O>;
 };
