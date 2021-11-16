a48d2f8322122ed080e6edf43d9b6dee0079d19b
Miroslav Vršecký <office@yamiteru.xyz>
2021-11-17 00:22:44 +0100

    Provide default types

--- src/primitives/derivedStream.ts
+++ src/primitives/derivedStream.ts
@@ -1,16 +1,16 @@
 import { stream } from ".";
 import derivedAny from "../help/derivedAny";
-import { Map, Maybe, Observable, ObservableOutputs, Some, Subs } from "../types";
+import { Map, Maybe, Observable, ObservableOutputs, Some, Stream, Subs } from "../types";
 
 export const derivedStream = <
     O extends Some,
-    A extends Observable<any, any>[],
-    I extends ObservableOutputs<A>,
+    A extends Observable<any, any>[] = Observable<any, any>[],
+    I extends ObservableOutputs<A> = ObservableOutputs<A>,
 >(...props: [
     observables: [...A],
     map: Map<I, O, undefined>,
     subs?: Maybe<Subs<O>>
-]) => derivedAny<O, A, I>(
+]) => derivedAny<O, A, I, Stream>(
     stream, 
     undefined,
     ...props
--- src/primitives/derivedValue.ts
+++ src/primitives/derivedValue.ts
@@ -1,17 +1,17 @@
 import { value } from ".";
 import derivedAny from "../help/derivedAny";
-import { Map, Maybe, Observable, ObservableOutputs, Some, Subs } from "../types";
+import { Map, Maybe, Observable, ObservableOutputs, Some, Subs, Value } from "../types";
 
 export const derivedValue = <
     O extends Some,
-    A extends Observable<any, any>[],
-    I extends ObservableOutputs<A>,
+    A extends Observable<any, any>[] = Observable<any, any>[],
+    I extends ObservableOutputs<A> = ObservableOutputs<A>,
 >(...props: [
     initial: O,
     observables: [...A],
     map: Map<I, O, O>,
     subs?: Maybe<Subs<O>>
-]) => derivedAny<O, A, I>(
+]) => derivedAny<O, A, I, Value>(
     value, 
     ...props
 );
