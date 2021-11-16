670e2535c1d81a4d339062046ebadad26a00e8ec
Miroslav Vršecký <office@yamiteru.xyz>
2021-11-17 00:22:28 +0100

    Create instance types

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,23 @@
-import { stream } from "./primitives/stream";
-import { value } from "./primitives/value";
+import { stream } from "./primitives";
+import { value } from "./primitives";
 
 export type Maybe<T> = T | null;
 export type Some = string | number | boolean | symbol | object | null;
 export type Noop = () => void;
 
-export type Value = typeof value;
 export type Stream = typeof stream;
+export type StreamInstance<I, O = I> = {
+  next: (v: I) => void,
+  onNext: (cb: Callback<O, void>) => Noop,
+  error: (v: string) => void,
+  onError: (cb: Callback<string, void>) => Noop,
+  end: Noop,
+  onEnd: (cb: Callback<void, void>) => Noop,
+};
+export type Value = typeof value;
+export type ValueInstance<I, O = I> = StreamInstance<I, O> & {
+  get: () => O,
+};
 
 export type Map<I extends Some, O extends Some, T> = (next: I, latest: T) => O | undefined;
 export type Callback<T, O = void> = (value: T) => O;
